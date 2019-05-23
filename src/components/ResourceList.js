37dde31a43c54ec270fa9b5de83680d867f73659
Elena Granados <patty518@yahoo.com>
2019-05-22 20:30:30 -0700

    initial commit

---
+++ src/components/ResourceList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import useResources from './useResources';
+
+
+
+
+const ResourceList = ({resource}) => {
+  console.log(resource);
+  const resources = useResources(resource);
+//   const [ resources, setResources ] = useState([]);
+
+// //  const fetchResource = async (resource) => {
+// //     const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+    
+// //     setResources(response.data);
+// //   }
+
+//   useEffect (
+//     () => {
+//       (async resource => {
+//         const response = await axios.get(
+//           `https://jsonplaceholder.typicode.com/${resource}`
+//         );
+        
+//         setResources(response.data);
+//       })(resource);
+//     },
+//     [resource]
+//   );
+
+  
+  return (
+  <ul>
+    {resources.map(record => <li key={record.id}>{resource ==='users'? record.name: record.title}</li>)}
+  </ul>)
+}
+
+export default ResourceList;
+
